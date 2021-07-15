1e13f0f060e666ef1b07a71150a630d471bffc14
jrozal <68182930+jrozal@users.noreply.github.com>
2021-07-15 09:46:53 -0700

    Add new socket.io event listeners

--- client/src/socket.js
+++ client/src/socket.js
@@ -4,6 +4,7 @@ import {
   setNewMessage,
   removeOfflineUser,
   addOnlineUser,
+  addConversation,
 } from "./store/conversations";
 
 const socket = io(window.location.origin);
@@ -22,6 +23,19 @@ socket.on("remove-offline-user", (id) => {
 
 socket.on("new-message", (data) => {
   store.dispatch(setNewMessage(data.message, data.sender));
+});
+
+socket.on("new-conversation", (data) => {
+  console.log('new convo DATA', data)
+  store.dispatch(addConversation(data.recipientId, data.message));
+});
+
+socket.on("new-message-error", (error) => {
+  console.error('New message error', error)
+});
+
+socket.on("error", (error) => {
+  console.error('Error', error)
 });
 
 export default socket;
