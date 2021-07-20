b422656ba867b73194e4ddfac5363f6289e8526c
jrozal <68182930+jrozal@users.noreply.github.com>
2021-07-19 17:55:55 -0700

    Reinstate removed comments

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -92,6 +92,8 @@ const sendMessage = (data, body) => {
   });
 };
 
+// message format to send: {recipientId, text, conversationId}
+// conversationId will be set to null if its a brand new conversation
 export const postMessage = (body) => async (dispatch) => {
   try {
     const data = await saveMessage(body);
@@ -107,7 +109,6 @@ export const postMessage = (body) => async (dispatch) => {
     console.error(error);
   }
 };
-
 
 export const searchUsers = (searchTerm) => async (dispatch) => {
   try {
